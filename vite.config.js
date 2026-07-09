ec278b90fa50511c5f9ed8077de77aeeb686782c
Raheem ullah <raheemullah@Raheems-MacBook-Pro.local>
2026-07-09 11:29:35 +0500

    created thank you page

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   css: {
     transformer: 'lightningcss',
   },
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, 'index.html'),
+        thankYou: resolve(import.meta.dirname, 'thank-you/index.html'),
+      },
+    },
     cssMinify: 'lightningcss',
     minify: 'terser',
   },
